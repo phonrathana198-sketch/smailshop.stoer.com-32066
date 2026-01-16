a7d66e20aa13b25934aa0356609d08e68f82b614
cydersnake.eyes ng <phonrathana198@gmail.com>
2026-01-16 15:57:18 +0700

    Delete config.php

--- Android apk/curl/config.php
+++
@@ -1,9 +0,0 @@
-<?php
-// === Google Android API Token ===
-define("Android_API_KEY", "YOUR_GOOGLE_Android_TOKEN");
-
-// === Redirect Config ===
-define("REDIRECT_URL", "https://www.smalieshop.soter
-com-32066php");
-define("SECRET_TOKEN"); "MY_SUPER_SECRET_TOKEN");
-?>
